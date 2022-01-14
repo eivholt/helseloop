79404961bec09b885fc1084bdba4500995bca644
André Schjøth <aschjoth@gmail.com>
2022-01-14 17:27:52 +0100

    feat(): implemented appointmentpage

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import { Routes, Route } from 'react-router';
 import './App.css';
+import Appointment from './pages/Appointment';
 
 function App() {
-    return <div className='App'></div>;
+	return (
+		<div className='App'>
+			<Routes>
+				<Route path='/' element={<Appointment />} />
+				{/* <Route path="about" element={< />} /> */}
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/pages/Appointment.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Link from "react-router";
+
+interface AppointmentType {
+	title: string;
+	date: Date;
+	instruction: string;
+	address: string;
+}
+
+const AppointmentData: AppointmentType = {
+	title: "Avtale med Medisinsk avdeling til Nordlandssykehuset",
+	date: new Date(2022, 6, 10, 10, 30),
+	instruction: "Se innkallingsbrev",
+	address: "Behandlingsområde K3 i 3. etasje, Parkveien 95 8093 Bodø",
+};
+
+function Appointment() {
+	return (
+		<div>
+			<h1>Innkalling til time</h1>
+			<h2>{AppointmentData.title}</h2>
+
+			<h3>Tid:</h3>
+			<p>{AppointmentData.date}</p>
+
+			<h3>Oppmøte:</h3>
+			<p>{AppointmentData.instruction}</p>
+
+			<h3>Sted:</h3>
+			<p>{AppointmentData.address}</p>
+
+			<Link to='./routeplanner'>Apne reiseplanlegger</Link>
+		</div>
+	);
+}
+
+export default Appointment;

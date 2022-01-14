59c34d6e8f0e0a6dd251934720e2895192ed4b97
André Schjøth <aschjoth@gmail.com>
2022-01-14 17:28:27 +0100

    run lint --fix

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,14 @@ import './App.css';
 import Appointment from './pages/Appointment';
 
 function App() {
-	return (
-		<div className='App'>
-			<Routes>
-				<Route path='/' element={<Appointment />} />
-				{/* <Route path="about" element={< />} /> */}
-			</Routes>
-		</div>
-	);
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Appointment />} />
+        {/* <Route path="about" element={< />} /> */}
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
